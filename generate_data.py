bf8e3167ae27eb3960d03d97809e81c7d2bea61f
Sharon Zhang <zsharonx@gmail.com>
2020-05-12 19:31:37 -0400

    add files

---
+++ generate_data.py
@@ -0,0 +1,395 @@
+import time
+import sys
+import json
+
+import numpy as np
+import scipy
+from scipy.spatial.distance import pdist, squareform
+from sklearn.utils.extmath import randomized_svd
+from sklearn.cluster import SpectralClustering
+
+
+###
+# GENERATE RANDOM DATA
+###
+
+def generate_data(l1, l2, n_samples=10000, seed=0, datatype='line_line'):
+  '''
+  generates uniform random data
+
+  l1: the first length parameter
+  l2: the second length parameter
+  num_samples: the number of samples to generate
+  seed: the random seed for generating the data
+  type: the type of data to generate
+  '''
+
+  np.random.seed(seed)
+
+  if datatype=='line_line':
+    # two line segments
+    line1_data = l1 * np.random.rand(n_samples)
+    line2_data = l2 * np.random.rand(n_samples)
+    data = np.column_stack((line1_data, line2_data))
+    data = np.row_stack(([l1, l2], data))
+
+  elif datatype=='line_circle':
+    # line segment and circle
+    line_data = l1 * np.random.rand(n_samples)
+    circle_data = np.empty((n_samples,2))
+    for i in range(n_samples):
+      theta = 2 * np.pi * np.random.rand()
+      circle_data[i,:] = [l2 * np.cos(theta), l2 * np.sin(theta)]
+    data = np.column_stack((line_data, circle_data))
+    data = np.row_stack(([l1, l2, 0], data))
+
+  elif datatype=='circle_circle':
+    # circle and circle
+    circleA_data = np.empty((n_samples,2))
+    circleB_data = np.empty((n_samples,2))
+    for i in range(n_samples):
+      theta = 2 * np.pi * np.random.rand()
+      circleA_data[i,:] = [l1 * np.cos(theta), l1 * np.sin(theta)]
+      theta = 2 * np.pi * np.random.rand()
+      circleB_data[i,:] = [l2 * np.cos(theta), l2 * np.sin(theta)]
+    data = np.column_stack((circleA_data, circleB_data))
+    data = np.row_stack(([l1, l2, 0, 0], data))
+
+  elif datatype=='rect_circle':
+    # rectangle and circle
+    line1_data = l1 * np.random.rand(n_samples)
+    line2_data = l2 * np.random.rand(n_samples)
+    rect_data = np.column_stack((line1_data, line2_data))
+
+    circle_data = np.empty((n_samples,2))
+    for i in range(n_samples):
+      theta = 2 * np.pi * np.random.rand()
+      circle_data[i,:] = [l1 * np.cos(theta), l1 * np.sin(theta)]
+    data = np.column_stack((rect_data, circle_data))
+    data = np.row_stack(([l1, l2, 0, 0], data))
+
+  else:
+    print('Error: invalid data type')
+    return
+
+  return data
+
+
+###
+# COMPUTE EIGENVECTORS
+###
+
+def calc_W(data, sigma=None):
+  '''
+  calculates the weight matrix W
+  '''
+
+  if sigma is None:
+    sigma = 1
+
+  t0 = time.perf_counter()
+  pairwise_sq_dists = squareform(pdist(data, 'sqeuclidean'))
+  W = np.exp(-pairwise_sq_dists / 2*sigma)
+  t1 = time.perf_counter()
+  print("  Calculating W took %2.2f seconds" % (t1-t0))
+
+  return W
+
+def calc_vars(data, W, n_comps=100):
+  '''
+  calculates phi, psi, and Sigma
+  '''
+
+  # calculate D, M, S
+  t0 = time.perf_counter()
+  D = np.zeros(data.shape[0])
+  for i in range(D.shape[0]):
+    D[i] = np.sum(W[i,:])
+  D_ = np.diag(D)
+  D_inv = scipy.linalg.inv(D_)
+  D_sqrt = np.sqrt(D_)
+  D_sqrt_inv = scipy.linalg.inv(D_sqrt)
+
+  M = D_inv @ W
+  S = D_sqrt @ M @ D_sqrt_inv
+  t1 = time.perf_counter()
+  print("  Calculating variables took %2.2f seconds" % (t1-t0))
+
+  # use svd
+  t0 = time.perf_counter()
+  V, Sigma, VT = randomized_svd(S,
+                                n_components=n_comps+1,
+                                n_iter=5,
+                                random_state=None)
+  t1 = time.perf_counter()
+  print("  Calculating svd took %2.2f seconds" % (t1-t0))
+
+  # calculate phi, psi
+  phi = D_sqrt_inv @ V
+  psi = D_sqrt @ V
+
+  return phi, Sigma
+
+
+###
+# FIND BEST EIGENVECTOR TRIPLETS
+###
+
+def scale(v1, v2):
+  '''
+  scales v1 to match the range of v2
+  '''
+
+  vs = (np.max(v2) - np.min(v2))/(np.max(v1) - np.min(v1)) * (v1 - np.min(v1)) + np.min(v2)
+  return vs
+
+def calculate_score(data, v1, v2):
+  '''
+  calculates proximity of v2 to v1
+  '''
+
+  # scale v1, v2 to [-1,1]
+  vs1 = scale(v1, [-1,1])
+  vs2 = scale(v2, [-1,1])
+
+  # calculate L1 distance between v1 and v2
+  score = np.linalg.norm((vs1 - vs2), ord=1) / vs1.shape[0]
+  return score
+
+def find_match(data, v, candidates):
+  '''
+  finds the best match to v out of candidates, according to score
+  returns the best match and its distance from v
+
+  candidates: an array of vectors (vertically concatenated)
+  '''
+
+  best_match = 0
+  best_dist = np.inf
+  for i in range(candidates.shape[1]):
+    u = candidates[:,i]
+
+    # test with positive
+    d = calculate_score(data, v, u)
+    if d < best_dist:
+      best_match = i
+      best_dist = d
+
+    # test with negative
+    d = calculate_score(data, v, -u)
+    if d < best_dist:
+      best_match = i
+      best_dist = d
+
+  return best_match, best_dist
+
+def find_matches(data, phi, n_eigenvectors=100):
+  '''
+  find all best triplets in the first n eigenvectors of the data
+  (excluding 0 eigenvector)
+  '''
+
+  t0 = time.perf_counter()
+  matches = np.zeros((n_eigenvectors, n_eigenvectors), dtype=int)
+  dists = np.zeros((n_eigenvectors, n_eigenvectors))
+  for i in range(1, n_eigenvectors+1):
+    for j in range(i+1, n_eigenvectors+1):
+      v1 = phi[:,i]
+      v2 = phi[:,j]
+      v = v1 * v2
+      match, dist = find_match(data, v, phi[:,j+1:])
+      matches[i-1,j-1] = match + j + 1
+      matches[j-1,i-1] = match + j + 1
+      dists[i-1,j-1] = dist
+      dists[j-1,i-1] = dist
+
+  t1 = time.perf_counter()
+  print("Finding best matches took %2.2f seconds" % (t1-t0))
+  return matches, dists
+
+def find_best_matches(phi, matches, dists, dist_thresh):
+  '''
+  returns a list of all matches satisfy the threshold
+  '''
+
+  best_matches = {}
+  best_dists = {}
+  for i in range(dists.shape[0]):
+    for j in range(i+1, dists.shape[0]):
+      if dists[i,j] < dist_thresh:
+        v1, v2, match = [i+1, j+1, matches[i,j]]
+        if match not in best_matches:
+          best_matches[match] = [v1, v2, match]
+          best_dists[match] = dists[i,j]
+        else:
+          if dists[i,j] < best_dists[match]:
+            best_matches[match] = [v1, v2, match]
+            best_dists[match] = dists[i,j]
+
+  return best_matches, best_dists
+
+
+###
+# VOTING SCHEME
+###
+def vote(edge_scores, triplet, dist):
+  '''
+  the scoring for scheme 3 is as follows:
+  when encountering a new triplet (t1, t2, t3):
+  place a negative vote for edge t1 - t2
+  (they are likely to be in opposite sets)
+  place a positive vote for edges t1 - t1 and t2 - t2
+  (they are likely to be eigenvectors of an independent manifold)
+  place a negative vote for edge t3 - t3
+  (t3 is unlikely to be an eigenvector of an independent manifold)
+  '''
+
+  t1, t2, t3 = [int(i) for i in triplet]
+  edge_scores[t1][t2] += np.exp(dist)
+  edge_scores[t2][t1] += np.exp(dist)
+  edge_scores[t1][t1] += 1
+  edge_scores[t2][t2] += 1
+  edge_scores[t3][t3] -= 1
+
+def get_votes(best_matches, best_dists, n_eigenvectors, K):
+  '''
+  gets edge votes for the triplets
+
+  returns an (n_eigenvectors x n_eigenvectors) numpy array of edges,
+  where n_eigenvectors is the number of eigenvectors being examined
+  '''
+
+  scores = np.zeros((n_eigenvectors, n_eigenvectors))
+  for triplet, dist in zip(best_matches, best_dists):
+    if (triplet[0] < n_eigenvectors and
+      triplet[1] < n_eigenvectors and
+      triplet[2] < n_eigenvectors):
+      vote(scores, triplet, dist)
+
+  edges = np.where(np.diag(scores>K))[0]
+  edge_scores = np.zeros((len(edges), len(edges)))
+  for i in range(edge_scores.shape[0]):
+    for j in range(edge_scores.shape[1]):
+      if i == j:
+        edge_scores[i][j] = 0
+      edge_scores[i][j] = scores[edges[i]][edges[j]]
+
+  edge_scores = np.exp(-edge_scores**2 / 2)
+  return edges, edge_scores
+
+def split_eigenvectors(edges, edge_scores):
+  '''
+  clusters eigenvectors into two separate groups
+  '''
+
+  clustering = SpectralClustering(n_clusters=2,  # default: 2
+                                  affinity='precomputed',
+                                  assign_labels='kmeans',
+                                  random_state=0).fit(edge_scores)
+
+  labels = np.zeros((2, len(edges)), dtype='int')
+  labels[0,:] = edges
+  labels[1,:] = clustering.labels_
+  return labels
+
+def main():
+  datafile = sys.argv[1]
+  with open(datafile) as f:
+    params = json.load(f)
+
+  # unpack parameters
+  print('Parameters...')
+  print(params)
+
+  name = params['name']
+  test_name = params['test_name']
+  precomputed = params['precomputed']
+  l1 = np.sqrt(np.pi) + params['l1']
+  l2 = params['l2']
+  n_samples = params['n_samples']
+  seed = params['seed']
+  datatype = params['datatype']
+  sigma = params['sigma']
+  n_comps = params['n_comps']
+  n_eigenvectors = params['n_eigenvectors']
+  K = params['K']
+  dist_thresh = params['dist_thresh']
+
+  # filenames
+  data_filename = '../data/data_' + name + '.dat'  # switch to path containing data
+  phi_filename = '../data/phi_' + name + '.dat'
+  Sigma_filename = '../data/Sigma_' + name + '.dat'
+  matches_filename = '../data/matches_' + name + '.dat'
+  dists_filename = '../data/dists_' + name + '.dat'
+
+  if precomputed:
+    # load data
+    print("\nLoading data...")
+    data = np.loadtxt(data_filename)[1:,:]
+
+    print("\nLoading phi, Sigma...")
+    phi = np.loadtxt(phi_filename)
+    Sigma = np.loadtxt(Sigma_filename)
+
+    print("\nLoading matches and distances...")
+    matches = np.loadtxt(matches_filename)
+    dists = np.loadtxt(dists_filename)
+  else:
+    # generate random data
+    print("\nGenerating random data...")
+    data = generate_data(l1, l2, n_samples=n_samples, seed=seed, datatype=datatype)
+    np.savetxt(data_filename, data)
+
+    # compute eigenvectors
+    print("\nComputing eigenvectors...")
+    W = calc_W(data, sigma=sigma)
+    phi, Sigma = calc_vars(data, W, n_comps=n_comps)
+
+    np.savetxt(phi_filename, phi)
+    np.savetxt(Sigma_filename, Sigma)
+
+    # find triplets
+    print("\nComputing triplets...")
+    matches, dists = find_matches(data, phi, n_eigenvectors)
+
+    np.savetxt(matches_filename, matches)
+    np.savetxt(dists_filename, dists)
+
+  # find best matches
+  print("\nFinding best matches...")
+  best_matches, best_dists = find_best_matches(phi, matches, dists, dist_thresh)
+
+  best_matches_list = []
+  best_dists_list = []
+  for match in list(best_matches):
+    best_matches_list.append(best_matches[match])
+    best_dists_list.append(best_dists[match])
+
+  dist_order = np.argsort(best_dists_list)
+  best_matches_list = [best_matches_list[i] for i in dist_order]
+  print('\n%d best matches found\n' % (len(best_matches)), best_matches_list)
+
+  # split eigenvectors
+  print("\nSplitting eigenvectors...")
+  eigenvectors, eigenvector_scores = get_votes(best_matches_list,
+                                               best_dists_list,
+                                               n_eigenvectors,
+                                               K)
+  labels = split_eigenvectors(eigenvectors, eigenvector_scores)
+
+  if labels[1][0] == 0:
+    manifold1 = labels[0][np.where(labels[1]==0)[0]]
+    manifold2 = labels[0][np.where(labels[1]==1)[0]]
+  else:
+    manifold2 = labels[0][np.where(labels[1]==0)[0]]
+    manifold1 = labels[0][np.where(labels[1]==1)[0]]
+
+  print("Manifold #1: ", manifold1)
+  print("Manifold #2: ", manifold2)
+
+  np.savetxt('manifold1_{}.dat'.format(test_name), manifold1)
+  np.savetxt('manifold2_{}.dat'.format(test_name), manifold2)
+
+
+if __name__ == '__main__':
+  main()
---
+++ generate_plots.py
@@ -0,0 +1,231 @@
+import time
+import sys
+import json
+
+import numpy as np
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+###
+# PLOT DATA
+###
+
+def plot_og_data(data, title=None, filename=None):
+  '''
+  plots the original data
+  '''
+
+  fig = plt.figure(figsize=(5,5))
+  if data.shape[1] <= 2:
+    ax = fig.add_subplot(111)
+    ax.scatter(data[:,0], data[:,1], s=5)
+  elif data.shape[1] == 3:
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter(data[:,0], data[:,1], data[:,2], s=5)
+  else:
+    ax = fig.add_subplot(111, projection='3d')
+    g = ax.scatter(data[:,0], data[:,1], data[:,2], c=data[:,3], s=5)
+    cb = plt.colorbar(g)
+  if title:
+    plt.title(title, pad=10)
+  if filename:
+    plt.savefig(filename)
+  plt.show()
+
+
+###
+# PLOT EIGENVECTORS
+###
+
+def plot_eigenvector(data, v, scaled=False, title=None, filename=None):
+  '''
+  plots the eigenvector v
+  if scale=True, then scale v to [0,1]
+  if index is specified, label the graph
+  '''
+
+  vs = scale(v, [0,1]) if scaled else v
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  g = ax.scatter(data[:,0], data[:,1], marker="s", c=vs[1:])
+  ax.set_aspect('equal', 'datalim')
+  cb = plt.colorbar(g)
+  if title:
+    plt.title(title)
+  if filename:
+    plt.savefig(filename)
+  plt.show()
+
+def plot_eigenvectors(data, eigenvectors, labels=None, title=None, filename=None):
+  '''
+  plots the array eigenvectors
+
+  eigenvectors: a list of eigenvectors
+  '''
+
+  rows = int(np.ceil(len(eigenvectors)**0.5))
+  cols = rows
+  if data.shape[1] <= 2:
+    fig, axs = plt.subplots(rows, cols, figsize=(5 * cols, 5 * rows))
+    for r in range(rows):
+      for c in range(cols):
+        ax = axs[r, c]
+        index = r * cols + c
+        if index >= len(eigenvectors):
+          ax.set_visible(False)
+        else:
+          v = eigenvectors[r * cols + c]
+          g = ax.scatter(data[:,0], data[:,1], marker="s", c=v[1:])
+          fig.colorbar(g, ax=ax)
+          ax.set_aspect('equal', 'datalim')
+          if labels is not None:
+            ax.set_title(labels[index])
+  else:
+    fig = plt.figure(figsize=(5 * cols, 5 * rows))
+    for r in range(rows):
+      for c in range(cols):
+        index = r * cols + c
+        ax = fig.add_subplot(rows, cols, index+1, projection='3d')
+        if index >= len(eigenvectors):
+          ax.set_visible(False)
+        else:
+          v = eigenvectors[r * cols + c]
+          g = ax.scatter(data[:,0], data[:,1], data[:,2], marker="s", c=v[1:])
+          fig.colorbar(g, ax=ax)
+          if labels is not None:
+            ax.set_title(labels[index])
+  if title:
+    plt.title(title)
+  if filename:
+    plt.savefig(filename)
+  plt.show()
+
+def plot_eigenvectors_combo(data, v1, v2, title=None, filename=None):
+  '''
+  plots the combination (element-wise multiplication) of vectors v1 and v2
+  '''
+
+  v = v1 * v2
+  plot_eigenvector(data, v, title, filename)
+
+def plot_independent_eigenvectors(manifold1, manifold2,
+                                  n_eigenvectors,
+                                  title=None, filename=None):
+  '''
+  plots a matrix with the independent eigenvectors color-coded by manifold
+  '''
+
+  # create separate manifolds
+  data = np.zeros((1,n_eigenvectors))
+  for i in manifold1:
+    data[0,int(i)] = 1
+  for j in manifold2:
+    data[0,int(j)] = -1
+
+  # create discrete colormap
+  if 1 in manifold1:
+    cmap = colors.ListedColormap(['blue', 'white', 'red'])
+  else:
+    cmap = colors.ListedColormap(['red', 'white', 'blue'])
+  bounds = [-0.5,-0.25,0.25,0.5]
+  norm = colors.BoundaryNorm(bounds, cmap.N)
+
+  fig, ax = plt.subplots()
+  ax.imshow(data, cmap=cmap, norm=norm, aspect='auto')
+
+  # draw gridlines
+  ax.axes.get_yaxis().set_visible(False)
+  ax.set_xticks(np.arange(0, n_eigenvectors, 5));
+
+  if title:
+    plt.title(title, fontsize=20)
+  if filename:
+    plt.savefig(filename)
+  plt.show()
+
+  plt.show()
+
+
+def main():
+  datafile = sys.argv[1]
+  with open(datafile) as f:
+    params = json.load(f)
+
+  # unpack parameters
+  print('Parameters...')
+  print(params)
+
+  name = params['name']
+  test_name = params['test_name']
+  precomputed = params['precomputed']
+  l1 = params['l1']
+  l2 = np.sqrt(np.pi) + params['l2']
+  n_samples = params['n_samples']
+  seed = params['seed']
+  datatype = params['datatype']
+  sigma = params['sigma']
+  n_comps = params['n_comps']
+  n_eigenvectors = params['n_eigenvectors']
+  K = params['K']
+  dist_thresh = params['dist_thresh']
+
+  # load data
+  data_filename = '../data/data_' + name + '.dat'
+  phi_filename = '../data/phi_' + name + '.dat'
+
+  data = np.loadtxt(data_filename)
+  l1, l2 = data[0,:2]
+  data = data[1:,:]
+  phi = np.loadtxt(phi_filename)
+
+  if datatype == 'line_circle':
+    data_r = np.zeros((data.shape[0],2))
+    data_r[:,0] = data[:,0]
+    data_r[:,1] = np.arctan2(data[:,2], data[:,1])
+    data = data_r
+  elif datatype == 'rect_circle':
+    data_r = np.zeros((data.shape[0],3))
+    data_r[:,0] = data[:,0]
+    data_r[:,1] = data[:,1]
+    data_r[:,2] = np.arctan2(data[:,3], data[:,2])
+    data = data_r
+
+  if not precomputed:
+    # plot original data
+    plot_og_data(data, title='Original Data ({})'.format(name), filename='original_data.png')
+
+    # plot eigenvectors
+    vecs = [phi[:,i] for i in range(1, n_eigenvectors+1)]
+    eigenvectors_filename = name + '_eigenvalues_' + str(n_eigenvectors) + '.png'
+    plot_eigenvectors(data,
+                      vecs[:20],
+                      labels=[int(i) for i in range(1,n_eigenvectors+1)],
+                      title='Laplace Eigenvectors ({})'.format(name),
+                      filename=eigenvectors_filename)
+
+  # plot best matches
+  manifold1 = np.loadtxt('../data/manifold1_{}.dat'.format(test_name))
+  manifold2 = np.loadtxt('../data/manifold2_{}.dat'.format(test_name))
+
+  vecs1 = [phi[:,int(i)] for i in manifold1]
+  vecs2 = [phi[:,int(i)] for i in manifold2]
+
+  plot_eigenvectors(data,
+                    vecs1,
+                    labels=[int(i) for i in manifold1],
+                    filename='manifold1_{}.png'.format(test_name))
+
+  plot_eigenvectors(data,
+                    vecs2,
+                    labels=[int(i) for i in manifold2],
+                    filename='manifold2_{}.png'.format(test_name))
+
+  plot_independent_eigenvectors(manifold1,
+                                manifold2,
+                                n_eigenvectors,
+                                title='d={}'.format(dist_thresh),
+                                filename='{}_{}_{}_eigenvector_division.png'.format(test_name, K, dist_thresh))
+
+if __name__ == '__main__':
+  main()
